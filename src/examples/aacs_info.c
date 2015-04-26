4e396e31cb6f6dff1c8675a9185792de3d4b9ef3
npzacs <npzacs@gmail.com>
2015-04-26 22:01:45 +0300

    Add MK to aacs_info

--- src/examples/aacs_info.c
+++ src/examples/aacs_info.c
@@ -110,6 +110,7 @@ int main (int argc, char **argv)
     }
 
     const uint8_t *vid = aacs_get_vid(aacs);
+    const uint8_t *mk  = aacs_get_mk(aacs);
     const uint8_t *id  = aacs_get_disc_id(aacs);
     const uint8_t *pmsn = aacs_get_pmsn(aacs);
     const int      bec  = aacs_get_bus_encryption(aacs);
@@ -119,6 +120,7 @@ int main (int argc, char **argv)
 
     printf("Disc ID: %s\n", id  ? _hex2str(id,  20) : "???");
     printf("VID    : %s\n", vid ? _hex2str(vid, 16) : "???");
+    printf("MK     : %s\n", vid ? _hex2str(mk, 16) : "???");
     printf("MKBv   : %d\n", aacs_get_mkb_version(aacs));
     printf("PMSN   : %s\n", pmsn ? _hex2str(pmsn, 16) : "???");
     printf("Bus encryption:\n");
